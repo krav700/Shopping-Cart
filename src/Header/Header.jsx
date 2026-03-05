c086ef404d5d10490406b1d609a77f99cd33ada3
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-03-05 18:26:36 +0200

    feat: add cart to Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import HeaderStyles from "./Header.module.css"
 
 function Header() {
   return (
     <>
       <h1>Header</h1>
-      <Link to="/">Home</Link>
-      <Link to="shop">Shop</Link>
-      <Link to="cart">Cart</Link>
+      <div className={HeaderStyles.body}>
+        <div className={HeaderStyles.links}>
+          <Link to="/" className={HeaderStyles.link}>Home</Link>
+          <Link to="shop" className={HeaderStyles.link}>Shop</Link>
+          <Link to="cart" className={HeaderStyles.link}>Cart</Link>
+        </div>
+        <div className={HeaderStyles.cart}>
+          <ShoppingCart size={40}/>
+          <span className={HeaderStyles["cart-items"]}>1</span>
+          </div>
+      </div>
     </>
   );
 }
