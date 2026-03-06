161f36eb49963720ae3ca02b7ca48791b93b30a9
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-03-06 22:31:45 +0200

    feat: add Context for cart items

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import "../index.css";
 import AppS from "./App.module.css";
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
+export const CartContext = createContext(null);
+
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div className={AppS.container}>
-      <Header />
-      <Outlet />
+      <CartContext value={{
+        cartItems,
+        setCartItems
+      }}>
+        <Header />
+        <Outlet />
+      </CartContext>
       <Footer />
     </div>
   );
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,24 +1,41 @@
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "lucide-react";
-import HeaderStyles from "./Header.module.css"
-
+import HeaderStyles from "./Header.module.css";
+import { CartContext } from "../App/App.jsx";
+import { useContext } from "react";
 function Header() {
-  return (
-    <>
-      <h1>Header</h1>
-      <div className={HeaderStyles.body}>
-        <div className={HeaderStyles.links}>
-          <Link to="/" className={HeaderStyles.link}>Home</Link>
-          <Link to="shop" className={HeaderStyles.link}>Shop</Link>
-          <Link to="cart" className={HeaderStyles.link}>Cart</Link>
-        </div>
-        <div className={HeaderStyles.cart}>
-          <ShoppingCart size={40}/>
-          <span className={HeaderStyles["cart-items"]}>1</span>
-          </div>
-      </div>
-    </>
-  );
+    const { cartItems, setCartItems } = useContext(CartContext);
+    let productCount = 0;
+    console.log(cartItems);
+    if (cartItems.length != 0) {
+      cartItems.map((product) => {
+        productCount += product.count;
+      });
+    }
+    return (
+        <>
+            <h1>Header</h1>
+            <div className={HeaderStyles.body}>
+                <div className={HeaderStyles.links}>
+                    <Link to="/" className={HeaderStyles.link}>
+                        Home
+                    </Link>
+                    <Link to="shop" className={HeaderStyles.link}>
+                        Shop
+                    </Link>
+                    <Link to="cart" className={HeaderStyles.link}>
+                        Cart
+                    </Link>
+                </div>
+                <div className={HeaderStyles.cart}>
+                    <ShoppingCart size={40} />
+                    <span className={HeaderStyles["cart-items"]}>
+                        {productCount}
+                    </span>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Header;
