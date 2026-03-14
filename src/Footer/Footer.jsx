71f65ea156f6e806ea3a07eb6a451b88dda82a24
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-03-14 13:44:32 +0200

    feat: add the footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router";
+import FooterStyles from "./Footer.module.css";
+
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className={FooterStyles["footer"]}>
+        <footer>
+          <h1>Footer</h1>
+          <div className={FooterStyles["content"]}>
+            <div className={FooterStyles.contact}>
+              <h2>Email: Shopaholic@gmail.com</h2>
+              <h2>Phone: 555-555-555</h2>
+              <h2>Address: Shopping Street 55</h2>
+            </div>
+            <Link className={FooterStyles.title}>Shopaholic</Link>
+          </div>
+        </footer>
+      </div>
+  )
 }
 
 export default Footer;
