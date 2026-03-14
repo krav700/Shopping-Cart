06e9da8a5a3a153a871350dec1f2e48f8b3ad7b0
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-03-14 10:43:43 +0200

    style: add colors and spacing to Card, Products and fix null error

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,44 +4,45 @@ import HeaderStyles from "./Header.module.css";
 import { CartContext } from "../App/App.jsx";
 import { useContext, useEffect, useState } from "react";
 function Header() {
-    const { cartItems } = useContext(CartContext);
+    const cartContext = useContext(CartContext);
+    const cartItems = cartContext?.cartItems ?? [];
     const [cartProductCount, setCartProductCount] = useState(0);
     useEffect(() => {
-      console.log("CART UPDATED2");
+        console.log("CART UPDATED2");
 
-      let cartProductCountSum = 0;
-      cartItems.map((product) => {
-        cartProductCountSum += Number(product.count);
-      });
-      setCartProductCount(cartProductCountSum);
-    }, [cartItems])
+        let cartProductCountSum = 0;
+        cartItems.map((product) => {
+            cartProductCountSum += Number(product.count);
+        });
+        setCartProductCount(cartProductCountSum);
+    }, [cartItems]);
     return (
-        <>
-            <h1>Header</h1>
+        <div className={HeaderStyles.header}>
+            <h1>🛍️ Shopaholic 🛍️</h1>
             <nav>
-              <div className={HeaderStyles.body}>
-                  <div className={HeaderStyles.links}>
-                      <Link to="/" className={HeaderStyles.link}>
-                          Home
-                      </Link>
-                      <Link to="shop" className={HeaderStyles.link}>
-                          Shop
-                      </Link>
-                      <Link to="cart" className={HeaderStyles.link}>
-                        Cart
-                      </Link>
-                  </div>
-                  <Link to="cart" className={HeaderStyles["cart-link"]}>
-                  <div className={HeaderStyles.cart}>
-                        <ShoppingCart size={40} />
-                        <span className={HeaderStyles["cart-items"]}>
-                            {cartProductCount}
-                        </span>
-                  </div>
-                  </Link>
-              </div>
+                <div className={HeaderStyles["nav-body"]}>
+                    <div className={HeaderStyles.links}>
+                        <Link to="/" className={HeaderStyles.link}>
+                            Home
+                        </Link>
+                        <Link to="shop" className={HeaderStyles.link}>
+                            Shop
+                        </Link>
+                        <Link to="cart" className={HeaderStyles.link}>
+                            Cart
+                        </Link>
+                    </div>
+                    <Link to="cart" className={HeaderStyles["cart-link"]}>
+                        <div className={HeaderStyles.cart}>
+                            <ShoppingCart size={40} />
+                            <span className={HeaderStyles["cart-items"]}>
+                                {cartProductCount}
+                            </span>
+                        </div>
+                    </Link>
+                </div>
             </nav>
-        </>
+        </div>
     );
 }
 
--- src/Products/Product.jsx
+++ src/Products/Product.jsx
@@ -1,41 +1,66 @@
 import ProductStyles from "./Product.module.css";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { CartContext } from "../App/App.jsx";
 import { Link } from "react-router";
 
-function Product({ style, title, image, price }) {
-    const { cartItems, setCartItems } = useContext(CartContext);
-    const [productCount, setProductCount] = useState(1);
+function Product({ style, title, image, price, quantity }) {
+    const cartContext = useContext(CartContext);
+    const cartItems = cartContext?.cartItems ?? [];
+    const setCartItems = cartContext?.setCartItems ?? null;
+    const productCount = useRef(1);
 
-    function addToCart() {
-        if (productCount == 0) {
-            alert("Please select more than 0 items!");
-            return;
-        }
-        if (cartItems.find((item) => item.title == title)) {
+    function addToCart(functionality) {
+        if (functionality != "cart") {
+            if (productCount.current == 0) {
+                alert("Please select more than 0 items!");
+                return;
+            }
+            if (cartItems.find((item) => item.title == title)) {
+                const cartItemsCopy = [...cartItems];
+                cartItemsCopy.map((item) => {
+                    if (item.title == title) {
+                        item.count += Number(productCount.current);
+                        return;
+                    }
+                });
+                setCartItems(cartItemsCopy);
+                return;
+            }
+
+            const productToAdd = {
+                title,
+                image,
+                price,
+                count: Number(productCount.current),
+            };
+            setCartItems((prev) => [...prev, productToAdd]);
+        } else {
             const cartItemsCopy = [...cartItems];
             cartItemsCopy.map((item) => {
                 if (item.title == title) {
-                    item.count += Number(productCount);
+                    item.count = Number(productCount.current);
                     return;
                 }
             });
             setCartItems(cartItemsCopy);
             return;
         }
+    }
 
-        const productToAdd = {
-            title,
-            image,
-            price,
-            count: Number(productCount),
-        };
-        setCartItems((prev) => [...prev, productToAdd]);
-        console.log("CART UPDATED");
+    function removeItem(title) {
+        let cartItemsCopy = [...cartItems];
+        cartItemsCopy = cartItemsCopy.filter((item) => item.title != title);
+        setCartItems(cartItemsCopy);
     }
 
     return (
-        <div className={style != "cart" ? ProductStyles.product : ProductStyles["cart-item"]}>
+        <div
+            className={
+                style != "cart"
+                    ? ProductStyles.product
+                    : ProductStyles["cart-item"]
+            }
+        >
             {style != "cart" ? (
                 <div className={ProductStyles["image-container"]}>
                     <img
@@ -58,7 +83,7 @@ function Product({ style, title, image, price }) {
                         <p>{Number(price).toFixed(2)} $</p>
                         <input
                             onChange={(e) => {
-                                setProductCount(e.target.value);
+                                productCount.current = e.target.value;
                             }}
                             className={ProductStyles["number-of-product"]}
                             type="number"
@@ -70,23 +95,38 @@ function Product({ style, title, image, price }) {
                     <div className={ProductStyles["buy-buttons"]}>
                         <button onClick={addToCart}>Add to Cart</button>
                         <Link to={"/cart"}>
-                            <button onClick={addToCart}>Checkout</button>
+                            <button
+                                onClick={addToCart}
+                                className={ProductStyles["checkout-btn"]}
+                            >
+                                Checkout
+                            </button>
                         </Link>
                     </div>
                 </div>
-
             ) : (
                 <>
                     <div>
-                        <h3 className={[ProductStyles["cart-title"]]}>{title}</h3>
+                        <button
+                            className={ProductStyles["remove-item-btn"]}
+                            onClick={() => {
+                                removeItem(title);
+                            }}
+                        >
+                            x
+                        </button>
+                        <h3 className={[ProductStyles["cart-title"]]}>
+                            {title}
+                        </h3>
                         <p>{Number(price).toFixed(2)}$</p>
                         <input
                             onChange={(e) => {
-                                setProductCount(e.target.value);
+                                productCount.current = e.target.value;
+                                addToCart("cart");
                             }}
+                            defaultValue={quantity}
                             className={ProductStyles["number-of-product"]}
                             type="number"
-                            placeholder={1}
                             min={0}
                             max={999}
                         />
